f7da04471fe9f3641b9caf9af19b0b670d73d1c8
daichi-okamura <daichi-okamura@bm-sms.co.jp>
2023-07-22 00:29:13 +0900

    button -> div

--- islands/NumberBox.tsx
+++ islands/NumberBox.tsx
@@ -1,46 +1,41 @@
 type NumberBoxProps = {
   label: string;
-  getter: () => string
+  getter: () => string;
   setter: (x) => void;
-}
+};
 
 export default function NumberBox(props: NumberBoxProps) {
   const { label, getter, setter } = props;
 
   return (
-    <div class="w-48">
-
-      <label class="text-gray-700 text-sm font-semibold">{ label }</label>
+    <>
+      <p class="text-gray-700 text-sm font-semibold m-1">{label}</p>
 
       <div class="flex flex-row flex-nowrap h-16">
-        <div>
-          <button
-            class="select-none bg-gray-200 hover:bg-gray-400 w-16 h-full rounded-l-xl cursor-pointer focus:outline-none"
-            onclick={ () => setter((prev) => Math.max(Number(prev) - 1, 1)) }
-          >
-            <span class="m-auto text-3xl font-thin">−</span>
-          </button>
+        <div
+          class="flex justify-center items-center w-16 select-none bg-gray-200 hover:bg-gray-400 rounded-l-xl cursor-pointer"
+          onclick={() => setter((prev) => Math.max(Number(prev) - 1, 1))}
+        >
+          <p class="text-center text-3xl font-thin">−</p>
         </div>
 
-        <div>
-          <input type="text"
-                 class="bg-gray-100 font-bold w-full h-full text-center text-2xl"
-                 value={ getter() }
-                 oninput={ (event) => setter(event.target.value) }
-                 disabled={ true }
+        <div class="flex justify-center items-center w-16">
+          <input
+            type="text"
+            class="bg-gray-100 font-bold h-full w-full text-center text-2xl"
+            value={getter()}
+            oninput={(event) => setter(event.target.value)}
+            disabled={true}
           />
         </div>
 
-        <div>
-          <button
-            class="select-none bg-gray-200 hover:bg-gray-400 w-16 h-full rounded-r-xl cursor-pointer focus:outline-none"
-            onclick={ () => setter((prev) => Number(prev) + 1) }
-          >
-            <span class="m-auto text-3xl font-thin">+</span>
-          </button>
+        <div
+          class="flex justify-center items-center w-16 select-none bg-gray-200 hover:bg-gray-400 rounded-r-xl cursor-pointer"
+          onclick={() => setter((prev) => Number(prev) + 1)}
+        >
+          <p class="text-center text-3xl font-thin">+</p>
         </div>
       </div>
-
-    </div>
+    </>
   );
 }
--- islands/NumberSelectors.tsx
+++ islands/NumberSelectors.tsx
@@ -1,16 +1,16 @@
 import { useRef, useState } from "preact/hooks";
+import { tw } from "twind";
 
 type NumberSelectorsProps = {
   label: string;
-  textColorOnSelected: string,
+  textColorOnSelected: string;
   current: string;
   setter: (x: number[]) => void;
-}
+};
 
 export default function NumberSelectors(props: NumberSelectorsProps) {
-
   const groupName = crypto.randomUUID();
-  const ref = useRef<HTMLInputElement>(null!)
+  const ref = useRef<HTMLInputElement>(null!);
   const { label, textColorOnSelected, current, setter } = props;
 
   function setNumber(number: string, checked: boolean): void {
@@ -18,35 +18,71 @@ export default function NumberSelectors(props: NumberSelectorsProps) {
     if (checked && index < 0) {
       setter(current + number);
     } else if (!checked && index >= 0) {
-      setter(current.replace(number, ''));
+      setter(current.replace(number, ""));
     }
   }
 
   return (
-    <div class="" ref={ ref }>
-      <label class="text-gray-700 text-sm font-semibold">{ label }</label>
+    <div class="" ref={ref}>
+      <p class="text-gray-700 text-sm font-semibold m-1">{label}</p>
       <div class="flex flex-wrap gap-x-1">
         <div class="flex gap-1">
-          <NumberSelector name={ groupName } number={ 1 } textColorOnSelected={ textColorOnSelected }
-                          numberChangeHandler={ setNumber }/>
-          <NumberSelector name={ groupName } number={ 2 } textColorOnSelected={ textColorOnSelected }
-                          numberChangeHandler={ setNumber }/>
-          <NumberSelector name={ groupName } number={ 3 } textColorOnSelected={ textColorOnSelected }
-                          numberChangeHandler={ setNumber }/>
-          <NumberSelector name={ groupName } number={ 4 } textColorOnSelected={ textColorOnSelected }
-                          numberChangeHandler={ setNumber }/>
-          <NumberSelector name={ groupName } number={ 5 } textColorOnSelected={ textColorOnSelected }
-                          numberChangeHandler={ setNumber }/>
+          <NumberSelector
+            name={groupName}
+            number={1}
+            textColorOnSelected={textColorOnSelected}
+            numberChangeHandler={setNumber}
+          />
+          <NumberSelector
+            name={groupName}
+            number={2}
+            textColorOnSelected={textColorOnSelected}
+            numberChangeHandler={setNumber}
+          />
+          <NumberSelector
+            name={groupName}
+            number={3}
+            textColorOnSelected={textColorOnSelected}
+            numberChangeHandler={setNumber}
+          />
+          <NumberSelector
+            name={groupName}
+            number={4}
+            textColorOnSelected={textColorOnSelected}
+            numberChangeHandler={setNumber}
+          />
+          <NumberSelector
+            name={groupName}
+            number={5}
+            textColorOnSelected={textColorOnSelected}
+            numberChangeHandler={setNumber}
+          />
         </div>
         <div class="flex gap-1">
-          <NumberSelector name={ groupName } number={ 6 } textColorOnSelected={ textColorOnSelected }
-                          numberChangeHandler={ setNumber }/>
-          <NumberSelector name={ groupName } number={ 7 } textColorOnSelected={ textColorOnSelected }
-                          numberChangeHandler={ setNumber }/>
-          <NumberSelector name={ groupName } number={ 8 } textColorOnSelected={ textColorOnSelected }
-                          numberChangeHandler={ setNumber }/>
-          <NumberSelector name={ groupName } number={ 9 } textColorOnSelected={ textColorOnSelected }
-                          numberChangeHandler={ setNumber }/>
+          <NumberSelector
+            name={groupName}
+            number={6}
+            textColorOnSelected={textColorOnSelected}
+            numberChangeHandler={setNumber}
+          />
+          <NumberSelector
+            name={groupName}
+            number={7}
+            textColorOnSelected={textColorOnSelected}
+            numberChangeHandler={setNumber}
+          />
+          <NumberSelector
+            name={groupName}
+            number={8}
+            textColorOnSelected={textColorOnSelected}
+            numberChangeHandler={setNumber}
+          />
+          <NumberSelector
+            name={groupName}
+            number={9}
+            textColorOnSelected={textColorOnSelected}
+            numberChangeHandler={setNumber}
+          />
         </div>
       </div>
     </div>
@@ -56,42 +92,45 @@ export default function NumberSelectors(props: NumberSelectorsProps) {
 type NumberSelectorProps = {
   name: string;
   number: number;
-  textColorOnSelected: string,
+  textColorOnSelected: string;
   numberChangeHandler: (number: number, checked: boolean) => void;
 };
 
 function NumberSelector(props: NumberSelectorProps) {
-
   const { name, number, textColorOnSelected, numberChangeHandler } = props;
-  const id = `${ name }-${ number }`;
-  const [ checked, setChecked ] = useState<boolean>(false);
+  const id = `${name}-${number}`;
+  const [checked, setChecked] = useState<boolean>(false);
 
   return (
-    <div class="flex">
-      <input type="checkbox"
-             id={ id }
-             name={ name }
-             value={ number }
-             class="hidden"
-             oninput={ (e) => {
-               const { target } = e;
-               setChecked(target.checked)
-               numberChangeHandler(target.value, target.checked);
-             } }
-      />
-      <label for={ id } class="rounded-xl border-2 border-gray-200">
-        <div class="inline-block w-[60px] h-[60px] select-none cursor-pointer">
-          { checked ? <>
-            <div class={ `mt-3 text-center text-2xl font-bold text-${ textColorOnSelected }-600` }>
-              { number }
+    <>
+      <div class="flex h-16 w-16">
+        <input
+          type="checkbox"
+          id={id}
+          name={name}
+          value={number}
+          class="hidden"
+          oninput={(e) => {
+            const { target } = e;
+            setChecked(target.checked);
+            numberChangeHandler(target.value, target.checked);
+          }}
+        />
+        <label
+          for={id}
+          class="flex justify-center items-center rounded-xl border-2 border-gray-200 w-full"
+        >
+          <div class="select-none cursor-pointer">
+            <div
+              class={tw`text-2xl font-bold ${
+                checked ? `text-${textColorOnSelected}-600` : "text-gray-200"
+              }`}
+            >
+              {number}
             </div>
-          </> : <>
-            <div class="mt-3 text-center text-2xl font-bold text-gray-200">
-              { number }
-            </div>
-          </> }
-        </div>
-      </label>
-    </div>
+          </div>
+        </label>
+      </div>
+    </>
   );
 }
