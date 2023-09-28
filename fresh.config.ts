21eedb46091205f222b4492e38fe868b7defe574
daichi-okamura <20348502+daichi-okamura@users.noreply.github.com>
2023-09-28 23:28:22 +0900

    change port to 9000

--- fresh.config.ts
+++ fresh.config.ts
@@ -3,5 +3,6 @@ import twindPlugin from "$fresh/plugins/twind.ts"
 import twindConfig from "./twind.config.ts";
 
 export default defineConfig({
-  plugins: [twindPlugin(twindConfig)]
+  plugins: [twindPlugin(twindConfig)],
+  port: 9000,
 });
